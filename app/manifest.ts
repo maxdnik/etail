303fb3bc379e1e4764af0babcccab622415f6bc6
maxdnik <65302701+maxdnik@users.noreply.github.com>
2026-07-28 16:03:09 -0300

    Update manifest description

--- app/manifest.ts
+++ app/manifest.ts
@@ -5,7 +5,7 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "ETA-IL Ayuda",
     short_name: "ETA-IL",
     description:
-      "Información y asistencia privada en español para preparar una solicitud ETA-IL para viajar a Israel.",
+      "Información y asistencia en español para preparar una solicitud ETA-IL para viajar a Israel.",
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
